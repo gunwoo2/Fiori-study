aa8f236395cf50adf1374c7a6658ce68e9e94839
Gunwoo <gguakim33@gmail.com>
2024-03-13 16:43:16 +0900

    modified:   exercise8/webapp/controller/Main.controller.js

--- exercise8/webapp/controller/Main.controller.js
+++ exercise8/webapp/controller/Main.controller.js
@@ -17,33 +17,40 @@ sap.ui.define([
                 oView.setModel(oModel); // JSON Model 정보를 현재 View에 등록
             },
 
-onButtonClick: function() {
-    sap.m.MessageToast.show("버튼을 클릭했습니다.");
-    // MessageToast.show("버튼을 클릭했습니다.");
+            onButtonClick: function() {
+                sap.m.MessageToast.show("버튼을 클릭했습니다.");
+                // MessageToast.show("버튼을 클릭했습니다.");
 
-    //Popup.fragment.xml 을 팝업창으로 호출하는 로직
-    let oView = this.getView();
-    let oDialog = this.byId("idDialog");
+                //Popup.fragment.xml 을 팝업창으로 호출하는 로직
+                let oView = this.getView();
+                let oDialog = this.byId("idDialog");
 
-    if ( oDialog ){
-        oDialog.open(); // Fragment를 Load 한 적이 있으면 byId로 찾을 수 있다.
-                        // 그렇게 찾은 Fragment를 팝업창으로 보여준다.
-    } else { 
-        // Popup.fragment.xml 파일을 읽어옴
-        // Controller도 연결함
-        let oFragment = sap.ui.core.Fragment.load({
-            id: oView.getId(),
-            name: "sync.e23.exercise8.view.Popup",
-            type: "XML",
-            controller: this
-        });
+                if ( oDialog ){
+                    oDialog.open(); // Fragment를 Load 한 적이 있으면 byId로 찾을 수 있다.
+                                    // 그렇게 찾은 Fragment를 팝업창으로 보여준다.
+                } else { 
+                    // Popup.fragment.xml 파일을 읽어옴
+                    // Controller도 연결함
+                    let oFragment = sap.ui.core.Fragment.load({
+                        id: oView.getId(),
+                        name: "sync.e23.exercise8.view.Popup",
+                        type: "XML",
+                        controller: this
+                    });
 
-        // Fragment Load 가 완료되면 Main View에 연결함 ( Main View의 모델도 이용 가능)
-        oFragment.then(function( oDialog ) {
-            oView.addDependent(oDialog); // View 에 연결
-            oDialog.open(); // 팝업창 출력
-        }); 
-    }
-}
-});
+                    // Fragment Load 가 완료되면 Main View에 연결함 ( Main View의 모델도 이용 가능)
+                    oFragment.then(function( oDialog ) {
+                        oView.addDependent(oDialog); // View 에 연결
+                        oDialog.open(); // 팝업창 출력
+                    }); 
+                }
+            },
+
+            onDialogClose: function() {
+                let oDialog = this.byId("idDialog");
+                if ( oDialog ){
+                    oDialog.close(); // 팝업창이 닫침.
+                }
+            }
+    });
 });
