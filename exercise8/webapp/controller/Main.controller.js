823c5dc3fb76d216fd5a5199f155b468da7260e4
Gunwoo <gguakim33@gmail.com>
2024-03-13 17:24:31 +0900

    modified:   exercise8/webapp/controller/Main.controller.js 	modified:   exercise8/webapp/view/Main.view.xml 	modified:   exercise8/webapp/view/Popup.fragment.xml

--- exercise8/webapp/controller/Main.controller.js
+++ exercise8/webapp/controller/Main.controller.js
@@ -24,8 +24,14 @@ sap.ui.define([
                 //Popup.fragment.xml 을 팝업창으로 호출하는 로직
                 let oView = this.getView();
                 let oDialog = this.byId("idDialog");
-
+                let oInput = this.byId("idInput"); // View에서 idInput인 id를 가진 Elemnet를 찾는다.
+                let name = oInput.getValue(); // 입력필드의 값을 Name에 저장한다.
+                
                 if ( oDialog ){
+                    // 팝업창의 텍스트 Element 중 id 가 idNAMEText 인 항목을 찾아서
+                    let oNameText = this.byId("idNAMEText");
+                    oNameText.setText(name); // 이름이 입력된 입력필드의 값을 전달함
+
                     oDialog.open(); // Fragment를 Load 한 적이 있으면 byId로 찾을 수 있다.
                                     // 그렇게 찾은 Fragment를 팝업창으로 보여준다.
                 } else { 
@@ -33,7 +39,7 @@ sap.ui.define([
                     // Controller도 연결함
                     let oFragment = sap.ui.core.Fragment.load({
                         id: oView.getId(),
-                        name: "sync.e23.exercise8.view.Popup",
+                        name: "sync.e03.exercise8.view.Popup",
                         type: "XML",
                         controller: this
                     });
@@ -41,6 +47,10 @@ sap.ui.define([
                     // Fragment Load 가 완료되면 Main View에 연결함 ( Main View의 모델도 이용 가능)
                     oFragment.then(function( oDialog ) {
                         oView.addDependent(oDialog); // View 에 연결
+
+                        let oNameText = oView.byId("idNAMEText");
+                        oNameText.setText(name);
+
                         oDialog.open(); // 팝업창 출력
                     }); 
                 }
