188e9783cb902ac6a39cd98bbfa4e60c6e7ea434
Gunwoo <gguakim33@gmail.com>
2024-03-15 20:15:16 +0900

    exercise 13

---
+++ exercise13/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sync.e03.exercise13.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ exercise13/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce03/exercise13/test/unit/controller/MAIN.controller"
+], function () {
+	"use strict";
+});
