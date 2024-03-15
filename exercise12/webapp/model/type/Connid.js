92e98a53a3cbd80f746202df7b68834accf8bbd1
Gunwoo <gguakim33@gmail.com>
2024-03-15 20:15:57 +0900

    exercise 12

--- exercise12/webapp/model/type/Connid.js
+++ exercise12/webapp/model/type/Connid.js
@@ -1,9 +1,9 @@
 sap.ui.define([
     "sap/ui/model/SimpleType",
-    "sap/ui/mode/VaildateException"
+    "sap/ui/model/ValidateException"
     ],
 
-    function ( SimpleType ) {
+    function ( SimpleType, ValidateException ) {
         "use strict";
         return SimpleType.extend("sync.e03.exercise12.model.type.Connid", {
 
@@ -12,9 +12,25 @@ sap.ui.define([
                 return oValue;
 
             },
-            
-            parseValueL: function (oValue) {
+
+            parseValue: function (oValue) {
                 return oValue;
-            }
+            },
+            
+            validateValue: function( oValue ) {
+                // /^ : 문자의 시작
+                // \d : 숫자
+                // \d{4} : 숫자 4개
+                // $/ : 문자의 종료
+                if ( /^\d{4}$/.test( oValue )  ) {
+                    // oValue는 숫자 4개로 구성되어 있다.
+                } else {
+                    // oValue는 숫자 4개로 구성되어 있지 않다.
+                    // 오류~!!
+                    // ABAP 에서는 raise exception 과 동일
+                    throw new ValidateException( "항공편은 숫자 4자리로 입력해주세요." );
+                }
+
+            } 
         });
     });
