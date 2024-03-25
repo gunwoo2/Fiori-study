a70a598ec2c530fe7ff89721010e24932ff0c124
Gunwoo <gguakim33@gmail.com>
2024-03-25 09:32:21 +0900

    수정 및 조회모드, 삭제 프로그램 만들기

---
+++ ex4/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/e03/ex4/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ex4/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce03/ex4/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
